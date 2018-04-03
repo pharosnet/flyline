43b8c1bbbdbbbe59272aea58a24b667c9c80fb42
Ryougi Nevermore <ryougi.nevermore@hotmail.com>
2018-04-03 17:22:27 +0800

    new sequence test
    add sequence test file, all green.

---
+++ sequence_test.go
@@ -0,0 +1,38 @@
+package flyline
+
+import (
+	"runtime"
+	"sync"
+	"testing"
+)
+
+func TestNewSequence(t *testing.T) {
+	seq := NewSequence()
+	if seq.Get() == -1 {
+		t.Logf("seq %v", seq)
+	} else {
+		t.Errorf("seq %v", seq)
+	}
+}
+
+func TestSequence_Incr(t *testing.T) {
+	seq := NewSequence()
+	v := seq.Get()
+	n := seq.Incr()
+	if n == v+1 {
+		t.Logf("seq incr: %v -> %v", v, n)
+	} else {
+		t.Errorf("seq incr: %v -> %v", v, n)
+	}
+	t.Log("thread safe testing....")
+	runtime.GOMAXPROCS(4)
+	wg := new(sync.WaitGroup)
+	for i := 0; i < 20; i++ {
+		wg.Add(1)
+		go func(seq *Sequence, wg *sync.WaitGroup) {
+			defer wg.Done()
+			t.Logf("seq now: %v incr : %v, get : %v", seq.Get(), seq.Incr(), seq.Get())
+		}(seq, wg)
+	}
+	wg.Wait()
+}

3830f96267f40204d96d555c4122b2afad637852
Ryougi Nevermore <ryougi.nevermore@hotmail.com>
2018-04-03 21:19:33 +0800

    update buffer.go
    add doc.

--- buffer.go
+++ buffer.go
@@ -2,7 +2,10 @@ package flyline
 
 // buffer interface
 type Buffer interface {
+	// Send item into buffer.
 	Send(i interface{}) error
-	Recv(i interface{}) (bool, error)
+	// Recv item from buffer, if closed eq true, then the buffer is closed and no remains.
+	Recv(i interface{}) (closed bool, err error)
+	// Recv original item from buffer, if closed eq true, then the buffer is closed and no remains.
 	RecvOri() (i interface{}, closed bool, err error)
 }

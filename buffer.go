489315f9b326914ed28e27b20b74df89449e0147
Ryougi Nevermore <ryougi.nevermore@hotmail.com>
2018-04-03 21:25:50 +0800

    update buffer.go
    add errors.

--- buffer.go
+++ buffer.go
@@ -1,5 +1,9 @@
 package flyline
 
+import (
+	"errors"
+)
+
 // buffer interface
 type Buffer interface {
 	// Send item into buffer.
@@ -9,3 +13,6 @@ type Buffer interface {
 	// Recv original item from buffer, if closed eq true, then the buffer is closed and no remains.
 	RecvOri() (i interface{}, closed bool, err error)
 }
+
+var ERR_BUF_SEND_CLOSED error = errors.New("cant not send item into the closed buffer")
+var ERR_BUF_RECV_CLOSED error = errors.New("cant not recv item from the closed buffer")

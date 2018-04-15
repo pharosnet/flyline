79c53f78d40c2c6f60928af7385ac30e12eb6741
ryougi.nevermore <ryougi.nevermore@hotmail.com>
2018-04-16 06:40:53 +0800

    NEW status_test.go
    status test cases.

---
+++ status_test.go
@@ -0,0 +1,11 @@
+package flyline
+
+import "testing"
+
+func TestStatus_Sample(t *testing.T)  {
+	s := new(status)
+	s.setRunning()
+	t.Logf("status running: %v", s.isRunning())
+	s.setClosed()
+	t.Logf("status close: %v", s.isClosed())
+}

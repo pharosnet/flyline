1b734d7d1ff6acc3c45bbb99d83248ab99b8fc52
ryougi.nevermore <ryougi.nevermore@hotmail.com>
2018-04-16 03:02:03 +0800

    UPDATE buffer.go
    add Close func.
    add Sync func.

--- buffer.go
+++ buffer.go
@@ -2,6 +2,7 @@ package flyline
 
 import (
 	"errors"
+	"context"
 )
 
 // buffer interface
@@ -10,6 +11,10 @@ type Buffer interface {
 	Send(i interface{}) error
 	// Recv value from buffer, if closed eq true, then the buffer is closed and no remains.
 	Recv() (value *Value, closed bool, err error)
+	// Close Buffer, when closed, can not send item into buffer, but can recv remains.
+	Cloes() (err error)
+	// Sync, waiting for remains to be received. Only can be called after Close().
+	Sync(ctx context.Context) (err error)
 }
 
 var ERR_BUF_SEND_CLOSED error = errors.New("cant not send item into the closed buffer")

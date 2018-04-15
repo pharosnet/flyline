e929ef11b30df13b9371ced9d130353fbb1acea3
ryougi.nevermore <ryougi.nevermore@hotmail.com>
2018-04-16 02:44:18 +0800

    UPDATE value.go
    change v to src.
    change logic of scan func.

--- value.go
+++ value.go
@@ -1,33 +1,43 @@
 package flyline
 
 import (
-	"reflect"
 	"fmt"
+	"reflect"
 )
 
 // Value Scanner
 type Value struct {
-	v interface{}
+	src interface{}
 }
 
 /*
  * Scan value
- * @param i ptr object
+ * @param dest ptr object
  *
  */
-func (v *Value) Scan(i interface{}) (err error) {
-	hosted := reflect.ValueOf(v.v)
-	promised := reflect.ValueOf(i)
-	if hosted.Kind() == reflect.Ptr {
-		if hosted.Kind() == reflect.Ptr {
-			promised.Elem().Set(hosted.Elem())
-			return
-		}
-		promised.Elem().Set(hosted)
-		return
-	} else if promised.Kind() == reflect.Slice || promised.Kind() == reflect.Map {
-		promised.Set(hosted)
+func (v *Value) Scan(dest interface{}) (err error) {
+	if dest == nil {
+		err = fmt.Errorf("dest is nil, dest = %v", dest)
 		return
 	}
-	return fmt.Errorf("not match, v = %v, i = %v", hosted.Kind(), promised.Kind())
+	dpv := reflect.ValueOf(dest)
+	if dpv.Kind() != reflect.Ptr {
+		err = fmt.Errorf("dest's type is not Ptr, dest = %v", dpv.Kind())
+		return
+	}
+	sv := reflect.ValueOf(v.src)
+	dv := reflect.Indirect(dpv)
+	if sv.Kind() == reflect.Ptr {
+		sv = sv.Elem()
+	}
+	if sv.IsValid() && sv.Type().AssignableTo(dv.Type()) {
+		dv.Set(sv)
+		return
+	}
+	if dv.Kind() == sv.Kind() && sv.Type().ConvertibleTo(dv.Type()) {
+		dv.Set(sv.Convert(dv.Type()))
+		return
+	}
+	err = fmt.Errorf("scan failed, not match, src = %v, dest = %v", sv.Type(), dv.Type())
+	return
 }

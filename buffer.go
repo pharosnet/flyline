ab669d3d0346b40d567d0f69d893693b66de66bd
RyougiNevermore <ryougi.nevermore@hotmail.com>
2018-04-23 09:22:18 +0800

    update buffer.go
    change recv() returns.

--- buffer.go
+++ buffer.go
@@ -14,7 +14,7 @@ type Buffer interface {
 	// Send item into buffer.
 	Send(i interface{}) (err error)
 	// Recv value from buffer, if active eq false, then the buffer is closed and no remains.
-	Recv() (value *Value, active bool)
+	Recv() (value interface{}, active bool)
 	// Get remains length
 	Len() (length int64)
 	// Close Buffer, when closed, can not send item into buffer, but can recv remains.

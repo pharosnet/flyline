5ce8fc9c0da5627ddbdb073058fff1977f9d77ea
RyougiNevermore <ryougi.nevermore@hotmail.com>
2018-04-23 10:03:50 +0800

    add array.
    add array and it's test case.

---
+++ array.go
@@ -0,0 +1,73 @@
+package flyline
+
+import (
+	"math"
+	"unsafe"
+)
+
+type entry struct {
+	value interface{}
+}
+
+func newArray(capacity int64) (a *array) {
+	if capacity > 0 && (capacity&(capacity-1)) != 0 {
+		panic("The array capacity must be a power of two, e.g. 2, 4, 8, 16, 32, 64, etc.")
+		return
+	}
+	var items []*entry = nil
+	align := int64(unsafe.Alignof(items))
+	mask := int64(capacity - 1)
+	shift := int64(math.Log2(float64(capacity)))
+	size := int64(capacity) * align
+	items = make([]*entry, size)
+	itemBasePtr := uintptr(unsafe.Pointer(&items[0]))
+	itemMSize := unsafe.Sizeof(items[0])
+	for i := int64(0); i < size; i++ {
+		items[i&mask*align] = new(entry)
+	}
+	return &array{
+		lhs:         [7]int64{},
+		capacity:    capacity,
+		size:        size,
+		shift:       shift,
+		align:       align,
+		mask:        mask,
+		items:       items,
+		itemBasePtr: itemBasePtr,
+		itemMSize:   itemMSize,
+		rhs:         [7]int64{},
+	}
+}
+
+// shared array
+type array struct {
+	lhs         [7]int64
+	capacity    int64
+	size        int64
+	shift       int64
+	align       int64
+	mask        int64
+	items       []*entry
+	itemBasePtr uintptr
+	itemMSize   uintptr
+	rhs         [7]int64
+}
+
+func (a *array) elementAt(seq int64) (e *entry) {
+	mask := a.mask
+	align := a.align
+	basePtr := a.itemBasePtr
+	mSize := a.itemMSize
+	entryPtr := basePtr + uintptr(seq&mask*align)*mSize
+	e = *((*(*entry))(unsafe.Pointer(entryPtr)))
+	return
+}
+
+func (a *array) set(seq int64, v interface{}) {
+	a.elementAt(seq).value = v
+}
+
+func (a *array) get(seq int64) (v interface{}) {
+	v = a.elementAt(seq).value
+	return
+}
